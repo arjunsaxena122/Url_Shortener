6bcf0a6382664f67803c9f0894b9efca49842110
Arjun Saxena <arjunsaxena122@gmail.com>
2024-11-26 16:36:15 +0000

    Add redis code

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 import express from "express"
+import redis, { Redis } from 'ioredis'
 
+
+const client = new Redis()
 const app = express()
 
 app.use(express.json({limit:'16kb'}))
@@ -9,8 +12,10 @@ app.use(express.urlencoded({extended:true,limit:'16kb'}))
 // Routers
 
 import urlRouter from "./routes/url.routes.js"
+import routerRedis from './routes/redis.js'
 
 app.use('/',urlRouter)
+app.use('/',routerRedis)
 
 
 // Error URl
@@ -19,4 +24,4 @@ app.use((req,res)=>{
     res.status(404).json({error:' This page is not valid'})
 })
 
-export default app
+export  {app, client}
